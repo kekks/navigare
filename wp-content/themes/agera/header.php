cb52b97e8ec08d0e77dd3ab9e7dd2bd858d8a58a
Francesco Chessa Orrù <francescochessaorru@gmail.com>
2013-07-14 11:23:40 +0200

    test to fix

--- wp-content/themes/agera/header.php
+++ wp-content/themes/agera/header.php
@@ -19,17 +19,10 @@
 		
 		
 	</head>
-<<<<<<< HEAD
 	<style>
 	#slogan h1 a{width:260px !important;}
 	#slogan h1 {font-size:17px !important;}
-=======
-	
-	<style>
-  .logo{width:661px;height:90px;}
-  
-  .logo img{margin:19px 5px;float:left;}
->>>>>>> 5efa20e3c4a299e70dccbae263330795af080daa
+
 	</style>
 
 	<body <?php body_class(); ?>>
@@ -45,19 +38,12 @@
 						echo '</ul>';
 					} ?> <!-- end menu --> 
 	
-<<<<<<< HEAD
+
 	     			 <div id="slogan">
 	    	  			<?php agera_add_logo(); ?>
 	  			    	<div style="float:left;"><a href="http://my-yacht-charter.com/charter-motor-yacht/"><img src="http://my-yacht-charter.com/wp-content/uploads/2013/07/mm.png"></a></div>
 	  			    	<div style="float:left;"><a href="http://my-yacht-charter.com/charter-sail-yacht/"><img src="http://my-yacht-charter.com/wp-content/uploads/2013/07/s.png"></a></div>  
 	  				</div> <!-- end slogan --> 
 	  			
-=======
-	     			 <div class="logo">
-	     			   <a href="http://my-yacht-charter.com"><img src="http://my-yacht-charter.com/wp-content/uploads/2013/04/Logo.png"></a>
-	     			   <a href="http://my-yacht-charter.com/charter-motor-yacht/"><img src="http://my-yacht-charter.com/wp-content/uploads/2013/07/m1.png"></a>
-	     			   <a href="http://my-yacht-charter.com/charter-sail-yacht/"><img src="http://my-yacht-charter.com/wp-content/uploads/2013/07/s1.png"></a>
-             </div> <!-- end slogan --> 
->>>>>>> 5efa20e3c4a299e70dccbae263330795af080daa
 	   			</div> <!-- end header -->
 			</div> <!-- end header-container -->
