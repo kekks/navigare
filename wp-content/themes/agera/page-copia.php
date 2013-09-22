7970988161577208392415268fc4310367c430d2
Francesco Chessa Orrù <francescochessaorru@gmail.com>
2013-09-22 20:02:45 +0200

    Facebook link added

--- wp-content/themes/agera/page-copia.php
+++ wp-content/themes/agera/page-copia.php
@@ -162,7 +162,7 @@ $page_id = get_the_ID();
   <div style="clear:both"></div>
   
   <div class="legal">
-   <p>Navigare Y.W. - Via Napoli, 108 - 09124 Cagliari (CA) Italy - tel. +39 070 6402523 - fax. 178 2203243 - cell. +39 3351436586 - PI 03173110929 - <a href="https://plus.google.com/106443940266608905581" rel="publisher">Google+</a><p>
+   <p>Navigare Y.W. - Via Napoli, 108 - 09124 Cagliari (CA) Italy - tel. +39 070 6402523 - fax. 178 2203243 - cell. +39 3351436586 - PI 03173110929 - <a href="https://plus.google.com/106443940266608905581" rel="publisher">Google+</a> - <a href="https://www.facebook.com/MyYachtCharter">Facebook</a><p>
   </div>
 
 </div>
